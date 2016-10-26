56e97667b500a36c398ac557b1222706900cdf4f
Gilberto <Gilberto@GILBERTOPLA403B>
2016-10-26 22:22:50 +0200

    arreglo en vectores (faltaba const) y clase matriz casi acabada

--- Shiro/Shiro/src/maths/vec4f.h
+++ Shiro/Shiro/src/maths/vec4f.h
@@ -8,13 +8,13 @@ namespace shiro {
 		vec4f();
 		vec4f(float x, float y, float z, float w);
 		vec4f(const vec4f &v);
-		friend vec4f operator+(vec4f l,const vec4f &v);
+		friend vec4f operator+(const vec4f l,const vec4f &v);
 		vec4f& operator+=(const vec4f &v);
-		vec4f operator-(const vec4f &v);
+		friend vec4f operator-(const vec4f l,const vec4f &v);
 		vec4f& operator-=(const vec4f &v);
-		friend vec4f operator*(vec4f l, const float m);
+		friend vec4f operator*(const vec4f l, const float m);
 		vec4f& operator*=(const int i);
-		friend vec4f operator/(vec4f l, const float m);
+		friend vec4f operator/(const vec4f l, const float m);
 		vec4f& operator/=(const int i);
 		friend std::ostream& operator<<(std::ostream& out, const vec4f& v);
 		float mod();
